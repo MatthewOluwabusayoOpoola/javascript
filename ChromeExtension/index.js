4ffa97d99d69e56707f529ecaec975c516ecdcd9
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2022-08-04 22:41:10 +0100

    some changes

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -3,18 +3,27 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromLocalStorage)
+ 
 
 inputBtn.addEventListener("click", function(){    
     myLead.push(inputEl.value)
     // console.log(myLead)
     inputEl.value = ""
+
+    
+    window.localStorage.setItem("myLeads", JSON.stringify(myLead))
     renderLead()
+
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLead(){
     let listItem = ''
     for (let i = 0; i < myLead.length; i++){
-        // listItem +=("<li><a href='"+ myLead[i] +"'target= '_blank'>"+ myLead[i] + "</a></li> ")
+        //  
         listItem += `
         <li>
             <a href='${myLead[i]}'target= '_blank'>
