8936defb89bd5027c7287bb839d08fa1c30da6d0
Matthew Oluwabusayo Opoola <initoluwa@gmail.com>
2022-08-06 12:39:42 +0100

    Completed

---
+++ ChromeExtension/README.md
@@ -0,0 +1,6 @@
+## A Lead Extension
+
+This App helps to track, store leads(Url)
+
+# ![Photo description ](img/extension.png)
+# ![Photo description ](img/extension2.png)
--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -4,43 +4,59 @@ const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage){
     myLead = leadsFromLocalStorage
-    renderLead()
+    render(myLead)
+}
+
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLead.push(tabs[0].url)    
+        localStorage.setItem("myLeads", JSON.stringify(myLead))
+        render(myLead)
+})    
+})
+
+function render(leads){
+    let listItem = ''
+    for (let i = 0; i < leads.length; i++){
+        //  
+        listItem += `
+        <li>
+            <a href='${leads[i]}'target= '_blank'>
+                ${leads[i]}
+            </a>
+        </li> `
+
+        
+    } 
+    
+ulEl.innerHTML = listItem
 }
 
 deleteBtn.addEventListener('dblclick', function(){
     localStorage.clear()
     myLead = []
-    renderLead()
+    render(myLead)
 })
  
 
-inputBtn.addEventListener("click", function(){    
-    myLead.push(inputEl.value)
-    // console.log(myLead)
-    inputEl.value = ""
+inputBtn.addEventListener("click", function(){ 
+    if (inputEl.value) {
+        myLead.push(inputEl.value)
+        // console.log(myLead)
+        inputEl.value = ""
 
     
-    window.localStorage.setItem("myLeads", JSON.stringify(myLead))
-    renderLead()
+        window.localStorage.setItem("myLeads", JSON.stringify(myLead))
+        render(myLead)
 
-    console.log(localStorage.getItem("myLeads"))
+        console.log(localStorage.getItem("myLeads"))
+    } 
+    
 })
 
-function renderLead(){
-    let listItem = ''
-    for (let i = 0; i < myLead.length; i++){
-        //  
-        listItem += `
-        <li>
-            <a href='${myLead[i]}'target= '_blank'>
-                ${myLead[i]}
-            </a>
-        </li> `
-    } 
 
-    ulEl.innerHTML = listItem
-
-}
